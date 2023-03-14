ec4caa22466df943187f848de8a398c320934475
Jordi Cenzano Ferret <jcenzano@fb.com>
2023-03-14 14:36:25 +0100

    Initial

---
+++ players.js
@@ -0,0 +1,33 @@
+const  playersData = [
+    {
+        "id:": 0,
+        "name": "Lionel Messi",
+        "born": "24/06/1987",
+        "rating": 99,
+        "photoPath": "./pics/0.jpg"
+    }, {
+        "id": 1,
+        "name": "Cristiano Ronaldo",
+        "born": "05/02/1985",
+        "rating": 99,
+        "photoPath": "./pics/1.jpg"
+    }, {
+        "id": 2,
+        "name": "Kilian Mbappé",
+        "born": "20/12/1998",
+        "rating": 97,
+        "photoPath": "./pics/2.jpg"
+    }, {
+        "id": 3,
+        "name": "Angel Di Maria",
+        "born": "14/02/1988",
+        "rating": 95,
+        "photoPath": "./pics/3.webp"
+    }, {
+        "id": 4,
+        "name": "Erling Haaland",
+        "born": "21/07/2000",
+        "rating": 92,
+        "photoPath": "./pics/4.webp"
+    }
+];
